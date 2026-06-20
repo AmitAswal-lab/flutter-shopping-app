784f2927d654f1d48bf8e72697d38310f26d4e46
Amit Aswal <gametheory284@gmail.com>
2026-06-20 20:57:51 +0530

    feat: scaffold blank-slate Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShoppingApp());
+}
+
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Shopping App')),
+      body: const Center(child: Text('Shopping App')),
+    );
+  }
+}
