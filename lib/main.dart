04d32dc2c63e2eebcf5ab7d8a733b272be77f925
Amit Aswal <gametheory284@gmail.com>
2026-06-21 08:13:56 +0530

    feat: provide Cart via ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'models/cart.dart';
 import 'screens/product_list_screen.dart';
 
 void main() {
@@ -11,11 +13,14 @@ class ShoppingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
-      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
-      home: const ProductListScreen(),
+    return ChangeNotifierProvider(
+      create: (_) => Cart(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping App',
+        theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+        home: const ProductListScreen(),
+      ),
     );
   }
 }
